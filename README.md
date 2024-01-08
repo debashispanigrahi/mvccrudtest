f483ac4fe8e88605ed6ee789e5619111fced3049
Debashis Panigrahi <dpdebashis24@gmail.com>
2024-01-08 12:14:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mvccrudtest
---
+++ mvccrudtest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvccrudtest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
